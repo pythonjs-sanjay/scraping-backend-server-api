c7e3749ff16909e617552ae74802b12acc3bed67
Sanjay Modi <modisanjay488@gmail.com>
2025-07-02 12:16:18 +0530

    minor chnages

--- api/google-play-lib.js
+++ api/google-play-lib.js
@@ -96,8 +96,8 @@ app.get("/appstore/suggest", async (req, res) => {
   res.json(suggestions);
 });
 
-// app.listen(PORT, () => {
-//   console.log(`API running on http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`API running on http://localhost:${PORT}`);
+});
 
 // /app-review/rating?appId=com.dxco.pandavszombies
